061700fffc240b779d59b12d1222d14a97d6067f
Alex4386 <psh010209@gmail.com>
2026-08-26 19:30:40 +0900

    chore: hack solution

--- src/components/DriveDetail.tsx
+++ src/components/DriveDetail.tsx
@@ -372,7 +372,7 @@ export function DriveDetail({
       <div
         className={cn(
           "flex min-h-0 flex-1 gap-2",
-          showMap ? "flex-col lg:flex-row lg:items-stretch" : "flex-col",
+          showMap ? "flex-col lg:flex-row lg:items-center" : "flex-col",
         )}
       >
         <div className="relative isolate z-0 flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden rounded-lg bg-black">
@@ -431,7 +431,7 @@ export function DriveDetail({
         </div>
 
         {showMap ? (
-          <div className="relative z-0 aspect-square w-full shrink-0 isolate overflow-hidden rounded-lg border lg:h-full lg:w-auto lg:max-w-full">
+          <div className="relative z-0 aspect-square w-full max-w-[320px] shrink-0 self-center overflow-hidden rounded-lg border lg:h-[min(100%,320px)] lg:w-auto lg:max-w-none">
             <DriveMap
               className="absolute inset-0"
               getPosition={() => {
